58af86d6e4e56aee94d4bb138f117c3b519c9478
arthur <arthur.desmonts@gmail.com>
2025-05-13 17:50:38 +0200

    NEW PROJECT : DEEZER HOMPE PAGE COPY -> State : Menu without responsive OK

---
+++ deezer/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Télécharge Deezer, notre app de streaming, crée ton compte et écoute ta musique gratuitement en accédant à des millions de titres, playlists et podcasts.">
+    <title>Deezer | Écoute de la musique en ligne | App de musique</title>
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+    <?php include 'header.html'; ?>
+    <?php include 'homePage.html'; ?>
+    <?php include 'footer.html'; ?>
+    <script src="js/script.js"></script>
+</body>
+</html>
