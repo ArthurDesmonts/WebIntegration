c7ed224644db083c118846057aa8c5b9ba6d5491
arthur <arthur.desmonts@gmail.com>
2025-05-14 20:25:21 +0200

    ADD the hifi part with the svg animated (follow the cursor)

--- deezer/README.md
+++ deezer/README.md
@@ -1,3 +1,8 @@
-6h15
+8h30
 
-16H30
+1er amélioration : 
+    -> Dans le JS, reset de l'animation du svg de la section hifi (animated-h2-section).
+    
+    Sur le site officiel, si le pointer sort de la div, l'animation n'est pas reset, ce qui peux donner un effet 'glitché'.
+
+    -> J'ai donc ajouter un translate de reset quand le pointer sort de la div pour éviter ce cas là.
---
+++ deezer/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const section = document.querySelector('.animated-h2-section');
+    const paths = document.querySelectorAll('.swirly-visual path');
+
+    section.addEventListener('mousemove', (e) => {
+        const { clientX } = e;
+        const { left, width } = section.getBoundingClientRect();
+        
+        const x = ((clientX - left) / width - 0.5) * 2;
+
+        paths.forEach(path => {
+            const dragRatio = path.style.getPropertyValue('--drag-ratio');
+            const moveX = x * 1000 * dragRatio;
+            
+            path.style.transform = `translateX(${moveX}px)`;
+        });
+    });
+
+    section.addEventListener('mouseleave', () => {
+        paths.forEach(path => {
+            path.style.transform = 'translate(0, 0)';
+        });
+    });
+});
