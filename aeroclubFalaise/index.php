2fa07367ac54297300119569c62f86affbe77c49
ArthurCoding <119744690+ArthurCoding@users.noreply.github.com>
2025-05-19 14:44:24 +0200

    ADD new file for aeroclub falaise project

---
+++ aeroclubFalaise/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="L'Aéroclub de Falaise est une association affiliée à la Fédération Française Aéronautique (FFA) dispensant l'intégralité des formations liées au pilotage d'un avion léger.">
+    <title>Aeroclub de Falaise</title>
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+    <?php include 'header.html'; ?>
+    <?php include 'index.html'; ?>
+    <?php include 'footer.html'; ?>
+</html>
